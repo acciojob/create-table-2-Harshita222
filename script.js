df2ed4fe0859a76e757b7848edd0ebcf74a7319b
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-06-17 23:01:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function createTable() {
     //Write your code here
-  
+	let btn = document.getElementById('myTable')
+	btn.addEventListner("click",function(){
+		
+  let pt1=prompt("input number of rows")
+lett pt2= prompt("Input number of columns")
+ let table = document.getElementById('myTable');
+
+		for(let i = 0; i < pt1; i++) {
+    let row = table.insertRow(i);
+    for(let j = 0; j < pt2; j++) {
+        let cell = row.insertCell(j);
+        cell.innerHTML = `Row-${i} Column-${j}`;
+    }
+    }
+	})
 }
